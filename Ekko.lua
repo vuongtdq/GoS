74d1ee5ba8efb9c4a5a14c5e55506dc72856ad30
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-27 09:21:35 +0100

    Update Ekko.lua

--- Ekko.lua
+++ Ekko.lua
@@ -1,4 +1,6 @@
-if GetObjectName(myHero) ~= "Ekko" then return end
+if GetObjectName(GetMyHero()) ~= "Ekko" then return end
+
+if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua - Go download it and save it Common!") return end
 
 local EkkoMenu = MenuConfig("Ekko", "Ekko")
 EkkoMenu:Menu("Combo", "Combo")
@@ -20,7 +22,7 @@ EkkoMenu.Killsteal:Boolean("R", "Killsteal with R", true)
 EkkoMenu:Menu("Misc", "Misc")
 if Ignite ~= nil then EkkoMenu.Misc:Boolean("AutoIgnite", "Auto Ignite", true) end
 EkkoMenu.Misc:Boolean("Autolvl", "Auto level", true)
-EkkoMenu.Misc:List("Autolvltable", "Priority", 1, {"Q-E-W", "Q-W-E", "E-Q-W"})
+EkkoMenu.Misc:DropDown("Autolvltable", "Priority", 1, {"Q-E-W", "Q-W-E", "E-Q-W"})
 
 EkkoMenu:Menu("Drawings", "Drawings")
 EkkoMenu.Drawings:Boolean("Q", "Draw Q Range", true)
