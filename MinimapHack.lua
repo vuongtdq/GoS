f9d6a88c3814e43ee97ff05c324959d1107d235b
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-28 20:14:26 +0100

    Create MinimapHack.lua

---
+++ MinimapHack.lua
@@ -0,0 +1,34 @@
+local unittraveled = 0
+local MissTimer = {}
+local MissSec = {}
+local ticks = {}
+ 
+OnTick(function(myHero)
+        for i,enemy in pairs(GetEnemyHeroes()) do
+                if IsVisible(enemy) == false and IsDead(enemy) == false and GetTeam(enemy) ~= GetTeam(myHero) then
+                        if ticks[i] == nil then
+                        ticks[i] = GetTickCount()
+                        end
+                        MissTimer[i] = GetTickCount() - ticks[i]                  
+                        MissSec[i] =  MissTimer[i]/1000
+                else
+                        ticks[i] = nil
+                        MissTimer[i] = nil
+                        MissSec[i] = 0
+                end
+        end
+end)
+
+OnDraw(function(myHero)
+    for i,enemy in pairs(GetEnemyHeroes()) do
+        if enemy ~= nil and GetTeam(enemy) ~= GetTeam(myHero) and IsDead(enemy) == false and IsVisible(enemy) == false then
+          if MissSec[i] == nil then
+          MissSec[i] = 0
+          end
+          unittraveled = GetMovementSpeed(enemy)*MissSec[i]
+          if unittraveled > 50 and unittraveled < 10000 then
+          DrawCircle(GetOrigin(enemy), unittraveled,1,0,0xffffffff)
+          end
+        end
+    end
+end)
