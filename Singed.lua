b4186062d879aa1f76d3452523f483dcaa086733
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-28 12:46:00 +0100

    Update Singed.lua

--- Singed.lua
+++ Singed.lua
@@ -1,5 +1,7 @@
 if GetObjectName(GetMyHero()) ~= "Singed" then return end
 
+if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua - Go download it and save it Common!") return end
+
 local SingedMenu = MenuConfig("Singed", "Singed")
 SingedMenu:Key("Q", "Q Exploit", string.byte("T"))
 
