96f519f772a37585e9978ccdca5ff68488b4668f
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-31 15:50:18 +0100

    Update Ryze.lua

--- Ryze.lua
+++ Ryze.lua
@@ -1,4 +1,4 @@
-if GetObjectName(myHero) ~= "Ryze" then return end
+if GetObjectName(GetMyHero()) ~= "Ryze" then return end
 
 local RyzeMenu = MenuConfig("Ryze", "Ryze")
 RyzeMenu:Menu("Combo", "Combo")
@@ -34,28 +34,35 @@ RyzeMenu.Drawings:Boolean("Q", "Draw Q Range", true)
 RyzeMenu.Drawings:Boolean("W", "Draw W Range", true)
 RyzeMenu.Drawings:Boolean("E", "Draw E Range", true)
 RyzeMenu.Drawings:ColorPick("color", "Color Picker", {255,255,255,0})
+
+OnDraw(function(myHero)
+local col = RyzeMenu.Drawings.color:Value()
+if RyzeMenu.Drawings.Q:Value() then DrawCircle(myHeroPos(),900,1,0,col) end
+if RyzeMenu.Drawings.W:Value() then DrawCircle(myHeroPos(),600,1,0,col) end
+if RyzeMenu.Drawings.E:Value() then DrawCircle(myHeroPos(),600,1,0,col) end
+end)
 	
 OnTick(function(myHero)
   if IOW:Mode() == "Combo" then
 	
         local target = GetCurrentTarget()
-	local Q2Pred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1400,250,900,55,false,true)
+	local Q2Pred = GetPredictionForPlayer(myHeroPos(),target,GetMoveSpeed(target),1400,250,900,55,false,true)
 
-	if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, 700) and RyzeMenu.Combo.R:Value() and GotBuff(myHero, "ryzepassivestack") == 4 then
+	if CanUseSpell(myHero, _R) == READY and ValidTarget(target, 700) and RyzeMenu.Combo.R:Value() and GotBuff(myHero, "ryzepassivestack") == 4 then
         CastSpell(_R)
 	end  
 	  
-	if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 600) and RyzeMenu.Combo.W:Value() then
+	if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 600) and RyzeMenu.Combo.W:Value() then
         CastTargetSpell(target, _W)
 	end
 		
-        if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 600) and RyzeMenu.Combo.E:Value() then
+        if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 600) and RyzeMenu.Combo.E:Value() then
         CastTargetSpell(target, _E)
 	end
 	
-	if CanUseSpell(myHero, _Q) == READY and Q2Pred.HitChance == 1 and GotBuff(myHero, "ryzepassivestack") > 3 or GotBuff(myHero, "ryzepassivecharged") > 0 and RyzeMenu.Combo.Q:Value() and GoS:ValidTarget(target, 900) then
+	if CanUseSpell(myHero, _Q) == READY and Q2Pred.HitChance == 1 and GotBuff(myHero, "ryzepassivestack") > 3 or GotBuff(myHero, "ryzepassivecharged") > 0 and RyzeMenu.Combo.Q:Value() and ValidTarget(target, 900) then
 	CastSkillShot(_Q,Q2Pred.PredPos.x,Q2Pred.PredPos.y,Q2Pred.PredPos.z)
-        elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and RyzeMenu.Combo.Q:Value() and GoS:ValidTarget(target, 900) then
+        elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and RyzeMenu.Combo.Q:Value() and ValidTarget(target, 900) then
         Cast(_Q,target)
 	end				
 		
@@ -64,67 +71,67 @@ OnTick(function(myHero)
   if IOW:Mode() == "Harass" then
 	
 	local target = GetCurrentTarget()
-	local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1400,250,900,55,true,true)
-	local Q2Pred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1400,250,900,55,false,true)
+	local QPred = GetPredictionForPlayer(myHeroPos(),target,GetMoveSpeed(target),1400,250,900,55,true,true)
+	local Q2Pred = GetPredictionForPlayer(myHeroPos(),target,GetMoveSpeed(target),1400,250,900,55,false,true)
 	 	  
-	if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 600) and RyzeMenu.Harass.W:Value() then
+	if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 600) and RyzeMenu.Harass.W:Value() then
         CastTargetSpell(target, _W)
 	end
       
-	if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 600) and RyzeMenu.Harass.E:Value() then
+	if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 600) and RyzeMenu.Harass.E:Value() then
         CastTargetSpell(target, _E)
 	end
 	
-	if CanUseSpell(myHero, _Q) == READY and Q2Pred.Hitchance == 1 and RyzeMenu.Harass.Q:Value() and GoS:ValidTarget(target, 900) then
+	if CanUseSpell(myHero, _Q) == READY and Q2Pred.Hitchance == 1 and RyzeMenu.Harass.Q:Value() and ValidTarget(target, 900) then
 	CastSkillShot(_Q,Q2Pred.PredPos.x,Q2Pred.PredPos.y,Q2Pred.PredPos.z)
-        elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and RyzeMenu.Harass.Q:Value() and GoS:ValidTarget(target, 900) then
+        elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and RyzeMenu.Harass.Q:Value() and ValidTarget(target, 900) then
 	CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	end
 	
   end 
 
-	for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+	for i,enemy in pairs(GetEnemyHeroes()) do
 	
-	        local QPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1400,250,900,55,true,true)
+	        local QPred = GetPredictionForPlayer(myHeroPos(),enemy,GetMoveSpeed(enemy),1400,250,900,55,true,true)
 		local ExtraDmg = 0
 		if GotBuff(myHero, "itemmagicshankcharge") == 100 then
 		ExtraDmg = ExtraDmg + 0.1*GetBonusAP(myHero) + 100
 		end
 		
 		if Ignite and RyzeMenu.Misc.Autoignite:Value() then
-                  if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
+                  if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and ValidTarget(enemy, 600) then
                   CastTargetSpell(enemy, Ignite)
                   end
                 end
 		
-		if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 900) and RyzeMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 25*GetCastLevel(myHero,_Q)+35+.55*GetBonusAP(myHero)+0.015*GetMaxMana(myHero)+0.005*GetCastLevel(myHero,_Q)*GetMaxMana(myHero) + ExtraDmg) then 
+		if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(enemy, 900) and RyzeMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, 25*GetCastLevel(myHero,_Q)+35+.55*GetBonusAP(myHero)+0.015*GetMaxMana(myHero)+0.005*GetCastLevel(myHero,_Q)*GetMaxMana(myHero) + ExtraDmg) then 
 		CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-		elseif CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(enemy, 600) and RyzeMenu.Killsteal.W:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 60+20*GetCastLevel(myHero,_W)+0.4*GetBonusAP(myHero)+0.025*GetMaxMana(myHero) + ExtraDmg) then
+		elseif CanUseSpell(myHero, _W) == READY and ValidTarget(enemy, 600) and RyzeMenu.Killsteal.W:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, 60+20*GetCastLevel(myHero,_W)+0.4*GetBonusAP(myHero)+0.025*GetMaxMana(myHero) + ExtraDmg) then
 		CastTargetSpell(enemy, _W)
-		elseif CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(enemy, 600) and RyzeMenu.Killsteal.E:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 20+16*GetCastLevel(myHero,_E)+0.2*GetBonusAP(myHero)+0.02*GetMaxMana(myHero) + ExtraDmg) then
+		elseif CanUseSpell(myHero, _E) == READY and ValidTarget(enemy, 600) and RyzeMenu.Killsteal.E:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, 20+16*GetCastLevel(myHero,_E)+0.2*GetBonusAP(myHero)+0.02*GetMaxMana(myHero) + ExtraDmg) then
 		CastTargetSpell(enemy, _E)
 		end
 		
 	end
 
-for _,mob in pairs(GoS:GetAllMinions(MINION_JUNGLE)) do
+for _,mob in pairs(minionManager.objects) do
 		
-        if IOW:Mode() == "LaneClear" then
+        if GetTeam(mob) == 300 and IOW:Mode() == "LaneClear" then
 		local mobPos = GetOrigin(mob)
 		
-		if CanUseSpell(myHero, _R) == READY and RyzeMenu.JungleClear.R:Value() and GotBuff(myHero, "ryzepassivestack") == 4 and GoS:ValidTarget(mob, 900) then
+		if CanUseSpell(myHero, _R) == READY and RyzeMenu.JungleClear.R:Value() and GotBuff(myHero, "ryzepassivestack") == 4 and ValidTarget(mob, 900) then
 		CastSpell(_R)
 		end
 		
-		if CanUseSpell(myHero, _W) == READY and RyzeMenu.JungleClear.W:Value() and GoS:ValidTarget(mob, 600) then
+		if CanUseSpell(myHero, _W) == READY and RyzeMenu.JungleClear.W:Value() and ValidTarget(mob, 600) then
 		CastTargetSpell(mob, _W)
 		end
 		
-		if CanUseSpell(myHero, _Q) == READY and RyzeMenu.JungleClear.Q:Value() and GoS:ValidTarget(mob, 900) then
+		if CanUseSpell(myHero, _Q) == READY and RyzeMenu.JungleClear.Q:Value() and ValidTarget(mob, 900) then
 		CastSkillShot(_Q, mobPos.x, mobPos.y, mobPos.z)
 		end
 		
-	        if CanUseSpell(myHero, _E) == READY and RyzeMenu.JungleClear.E:Value() and GoS:ValidTarget(mob, 600) then
+	        if CanUseSpell(myHero, _E) == READY and RyzeMenu.JungleClear.E:Value() and ValidTarget(mob, 600) then
 		CastTargetSpell(mob, _E)
 		end
 		
@@ -139,10 +146,6 @@ if RyzeMenu.Misc.Autolvl:Value() then
 LevelSpell(leveltable[GetLevel(myHero)])
 end
 
-if RyzeMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,900,1,128,0xff00ff00) end
-if RyzeMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,600,1,128,0xff00ff00) end
-if RyzeMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,600,1,128,0xff00ff00) end
-
 end)	
 
-GoS:AddGapcloseEvent(_W, 600, true) -- hi Copy-Pasters ^^
+AddGapcloseEvent(_W, 600, true)
