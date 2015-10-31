544f80e0aaaaba94fe1270fbdb2a24b810b55d8c
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-31 15:43:18 +0100

    Update TurnAround.lua

--- TurnAround.lua
+++ TurnAround.lua
@@ -8,6 +8,30 @@ lastRightClick = {x = nil, y = nil, z = nil}
 lastPos = {x = nil, y = nil, z = nil}
 
 PrintChat(" >> TurnAround Script loaded! <<")
+
+OnTick(function(myHero)
+  if Move = true and GetTickCount() - LastMove > 850 then
+  MoveToXYZ(lastRightClick)
+  Move = false
+  end
+  
+  if lastRightClick.x ~= nil then
+    if math.abs(player.x-lastRightClick.x) <= 75 and math.abs(player.z-lastRightClick.z) <= 75 then
+    lastRightClick.x = nil
+    lastRightClick.y = nil
+    lastRightClick.z = nil
+    end
+    if lastPos.x ~= nil and lastPos.x == GetOrigin(myHero).x and lastPos.y == GetOrigin(myHero).y and lastPos.z == GetOrigin(myHero).z then
+    lastRightClick.x = nil
+    lastRightClick.y = nil
+    lastRightClick.z = nil
+    end
+	
+    lastPos.x = GetOrigin(myHero).x
+    lastPos.y = GetOrigin(myHero).y
+    lastPos.y = GetOrigin(myHero).z
+  end
+end)
   
 OnProcessSpell(function(unit,spell)
   if GetTeam(unit) ~= GetTeam(myHero) then 
@@ -19,7 +43,7 @@ OnProcessSpell(function(unit,spell)
       Move = true
       LastMove = GetTickCount()
       end
-	end
+    end
 	
     if spellName == "MockingShout" and GetDistance(spell) <= 850 then
       local DodgeThat = Vector(myHero)+(Vector(myHero)-Vector(spell)):normalize()*(-100)
@@ -38,29 +62,5 @@ OnWndMsg(function(msg,wParam)
   if msg == WM_RBUTTONDOWN then
   local mousePos = GetMousePos()
   lastRightClick = {x = mousePos.x, y = mousePos.y, z = mousePos.z}
-  end
-end)
-     
-OnTick(function(myHero)
-  if Move = true and GetTickCount() - LastMove > 850 then
-  MoveToXYZ(lastRightClick)
-  Move = false
-  end
-  
-  if lastRightClick.x ~= nil then
-    if math.abs(player.x-lastRightClick.x) <= 75 and math.abs(player.z-lastRightClick.z) <= 75 then
-    lastRightClick.x = nil
-	  lastRightClick.y = nil
-    lastRightClick.z = nil
-    end
-    if lastPos.x ~= nil and lastPos.x == GetOrigin(myHero).x and lastPos.y == GetOrigin(myHero).y and lastPos.z == GetOrigin(myHero).z then
-    lastRightClick.x = nil
-	  lastRightClick.y = nil
-    lastRightClick.z = nil
-    end
-	
-    lastPos.x = GetOrigin(myHero).x
-    lastPos.y = GetOrigin(myHero).y
-	  lastPos.y = GetOrigin(myHero).z
   end
 end)
