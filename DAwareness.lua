ba9dc396dd61b16542eed94457fead1b7a09a363
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-11 22:54:37 +0100

    Update DAwareness.lua

--- DAwareness.lua
+++ DAwareness.lua
@@ -9,6 +9,43 @@ DAwareness.WayPoints.Allies:Boolean(GetObjectName(myHero).."WP", " "..GetObjectN
 --DAwareness.CD:Menu("Allies", "Ally Team")
 --DAwareness.CD:Menu("Enemies", "Enemy Team")
 --DAwareness.CD.Allies:Boolean(GetObjectName(myHero).."CD", " "..GetObjectName(myHero).." ", true)
+DAwareness:Menu("MiniMapHack", "MiniMap Hack")
+DAwareness.MiniMapHack:Boolean("Enabled", "Enabled", true)
+
+local unittraveled = 0
+local MissTimer = {}
+local MissSec = {}
+local ticks = {}
+ 
+OnTick(function(myHero)
+        for i,enemy in pairs(GetEnemyHeroes()) do
+                if IsVisible(enemy) == false and IsDead(enemy) == false then
+                        if ticks[i] == nil then
+                        ticks[i] = GetTickCount()
+                        end
+                        MissTimer[i] = GetTickCount() - ticks[i]                  
+                        MissSec[i] =  MissTimer[i]/1000
+                else
+                        ticks[i] = nil
+                        MissTimer[i] = nil
+                        MissSec[i] = 0
+                end
+        end
+end)
+
+OnDrawMinimap(function() 
+    for i,enemy in pairs(GetEnemyHeroes()) do
+        if DAwareness.MiniMapHack.Enabled:Value() and enemy ~= nil and not IsDead(enemy) and IsVisible(enemy) == false then
+          if MissSec[i] == nil then
+          MissSec[i] = 0
+          end
+          unittraveled = GetMoveSpeed(enemy)*MissSec[i]
+          if unittraveled > 50 and unittraveled < 7000 then
+          DrawCircleMinimap(GetOrigin(enemy), unittraveled,1,100,ARGB(50,255,0,255))
+          end
+        end
+    end
+end)
 
 DelayAction(function()
   for k,v in pairs(GetEnemyHeroes()) do
