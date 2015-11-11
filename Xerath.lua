3dd989b6fa8cac0b1a87d745c6ef9f32dd09f5a2
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-11 23:06:31 +0100

    Update Xerath.lua

--- Xerath.lua
+++ Xerath.lua
@@ -132,7 +132,7 @@ OnTick(function(myHero)
        local EPred = GetPredictionForPlayer(myHeroPos(),enemy,GetMoveSpeed(enemy),1400,250,975,60,true,true)
 	   
         if Ignite and XerathMenu.Misc.AutoIgnite:Value() then
-          if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and ValidTarget(enemy, 600) then
+          if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*3 and ValidTarget(enemy, 600) then
           CastTargetSpell(enemy, Ignite)
           end
         end
