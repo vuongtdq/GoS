4b49e7f5ff354a588afce40789f6b7223a8fd89a
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-11 23:05:49 +0100

    Update Sivir.lua

--- Sivir.lua
+++ Sivir.lua
@@ -93,7 +93,7 @@ OnTick(function(myHero)
         end
         
 	if Ignite and SivirMenu.Misc.AutoIgnite:Value() then
-          if IsReady(Ignite) and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and ValidTarget(enemy, 600) then
+          if IsReady(Ignite) and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*3 and ValidTarget(enemy, 600) then
           CastTargetSpell(enemy, Ignite)
           end
 	end
