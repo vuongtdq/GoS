848f92a1d19703669d24b3544a52622687f1cd62
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-11 23:02:38 +0100

    Update Jinx.lua

--- Jinx.lua
+++ Jinx.lua
@@ -161,7 +161,7 @@ end
         end
 		
 	if Ignite and JinxMenu.Misc.AutoIgnite:Value() then
-          if IsReady(Ignite) and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and ValidTarget(enemy, 600) then
+          if IsReady(Ignite) and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*3 and ValidTarget(enemy, 600) then
           CastTargetSpell(enemy, Ignite)
           end
         end
