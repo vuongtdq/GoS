53abf9760d8f66a92b50bf966f7daab906bc925e
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-15 22:58:21 +0100

    Update Ahri.lua

--- Ahri.lua
+++ Ahri.lua
@@ -4,7 +4,7 @@ if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua
 if not pcall( require, "Deftlib" ) then PrintChat("You are missing Deftlib.lua - Go download it and save it in Common!") return end
 if not pcall( require, "DamageLib" ) then PrintChat("You are missing DamageLib.lua - Go download it and save it in Common!") return end
 
-AutoUpdate("/D3ftsu/GoS/blob/master/Ahri.lua","/D3ftsu/GoS/blob/master/Ahri.version","Ahri.lua",1)
+AutoUpdate("/D3ftsu/GoS/master/Ahri.lua","/D3ftsu/GoS/master/Ahri.version","Ahri.lua",1)
 
 local AhriMenu = MenuConfig("Ahri", "Ahri")
 AhriMenu:Menu("Combo", "Combo")
