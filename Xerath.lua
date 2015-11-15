79f51c8eed17fc6eaa8c68683d748b85276c91e5
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-15 22:57:30 +0100

    Update Xerath.lua

--- Xerath.lua
+++ Xerath.lua
@@ -4,7 +4,7 @@ if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua
 if not pcall( require, "Deftlib" ) then PrintChat("You are missing Deftlib.lua - Go download it and save it in Common!") return end
 if not pcall( require, "DamageLib" ) then PrintChat("You are missing DamageLib.lua - Go download it and save it in Common!") return end
 
-AutoUpdate("/D3ftsu/GoS/blob/master/Xerath.lua","/D3ftsu/GoS/blob/master/Xerath.version","Xerath.lua",1)
+AutoUpdate("/D3ftsu/GoS/master/Xerath.lua","/D3ftsu/GoS/master/Xerath.version","Xerath.lua",1)
 
 local XerathMenu = MenuConfig("Xerath", "Xerath")
 XerathMenu:Menu("Combo", "Combo")
