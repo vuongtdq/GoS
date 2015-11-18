fc4e88288dadbdca03bfb1b1297b43d3f4d36628
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-18 23:08:11 +0100

    Update Baseult.lua

--- Baseult.lua
+++ Baseult.lua
@@ -1,9 +1,6 @@
-require("Inspired")
-require("IPrediction")
-
 if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua - Go download it and save it Common!") return end
 
-AutoUpdate("/D3ftsu/GoS/master/Baseult.lua","/D3ftsu/GoS/master/Baseult.version","Baseult.lua",2)
+AutoUpdate("/D3ftsu/GoS/master/Baseult.lua","/D3ftsu/GoS/master/Baseult.version","Baseult.lua",1)
 
 local BasePositions = {
      [SUMMONERS_RIFT] = {
@@ -45,7 +42,7 @@ local SpellData = {
         
         ["Jinx"] = {
 	Delay = 600,
-        MissileSpeed = 1700,
+        MissileSpeed = 1700
 	Damage = function(target) return CalcDamage(myHero, target, math.max(50*GetCastLevel(myHero, _R)+75+GetBonusDmg(myHero)+(0.05*GetCastLevel(myHero, _R)+0.2)*(GetMaxHP(target)-GetCurrentHP(target)))) end
         } 
 }
@@ -63,8 +60,6 @@ end
 	
 local Isrecalling = {}
 
-local mysprite = CreateSpriteFromFile("recalltracker.png")
-
 OnDraw(function()
 
 if BaseultMenu.RT:Value() then
@@ -73,19 +68,23 @@ if BaseultMenu.RT:Value() then
 	  local percent=math.floor(GetCurrentHP(recall.Champ)/GetMaxHP(recall.Champ)*100)
 	  local leftTime = recall.starttime - GetTickCount() + recall.info.totalTime
 	  if leftTime<0 then leftTime = 0 end
-	  if i>0 then FillRect(400,500+18-2,168,1,0xC0000000) end
-	    if recall.info.isStart and mysprite > 0 then
-	    DrawSprite(mysprite,1000,150,1,1,400,100,ARGB(255,255,255,255))
-		DrawText( Champ .. " (" .. percent .. "%" .. ")", 12, (250*leftTime/recall.info.totalTime)+1050, 150, percentToRGB(percent))
-	    FillRect(1089,178,250*leftTime/recall.info.totalTime,8,percentToRGB(percent))
+	  FillRect(400,500+18*i-2,168,18,0x50000000)
+	  if i>0 then FillRect(400,500+18*i-2,168,1,0xC0000000) end
+  	  DrawText(string.format("%s (%d%%)", Champ, percent), 14, 402, 500+18*i, percentToRGB(percent))
+	    if recall.info.isStart then
+	    DrawText(string.format("%.1fs", leftTime/1000), 14, 515, 500+18*i, percentToRGB(percent))
+	    FillRect(569,500+18*i, 300*leftTime/recall.info.totalTime,14,0x80000000)
 	    else
  	      if recall.killtime == nil then
 	        if recall.info.isFinish and not recall.info.isStart then
+		recall.result = "finished"
 		recall.killtime =  GetTickCount()+2000
 		elseif not recall.info.isFinish then
+	 	recall.result = "cancelled"
 	        recall.killtime =  GetTickCount()+2000
 		end
               end
+	      DrawText(recall.result, 14, 515, 500+18*i, percentToRGB(percent))
 	    end
 	    if recall.killtime~=nil and GetTickCount() > recall.killtime then
 	    Isrecalling[Champ] = nil
@@ -134,4 +133,3 @@ function percentToRGB(percent)
 	
     return 0xFF000000+g*0xFFFF+r*0xFF
 end
- 
