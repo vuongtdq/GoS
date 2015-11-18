716cd03adfc062855438d903e38baadacbf85d94
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-18 12:42:31 +0100

    Update Ashe.lua

--- Ashe.lua
+++ Ashe.lua
@@ -4,6 +4,8 @@ if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua
 if not pcall( require, "Deftlib" ) then PrintChat("You are missing Deftlib.lua - Go download it and save it in Common!") return end
 if not pcall( require, "DamageLib" ) then PrintChat("You are missing DamageLib.lua - Go download it and save it in Common!") return end
 
+AutoUpdate("/D3ftsu/GoS/master/Ashe.lua","/D3ftsu/GoS/master/Ashe.version","Ashe.lua",1)
+
 local AsheMenu = MenuConfig("Ashe", "Ashe")
 AsheMenu:Menu("Combo", "Combo")
 AsheMenu.Combo:Boolean("Q", "Use Q", true)
