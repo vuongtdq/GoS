1df059a7f76f9304d52802a7cc56f4c991d7d13b
D3ftsu <ahmedtayachon1@gmail.com>
2015-11-23 14:49:33 +0100

    Update Jinx.lua

--- Jinx.lua
+++ Jinx.lua
@@ -4,7 +4,7 @@ if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua
 if not pcall( require, "Deftlib" ) then PrintChat("You are missing Deftlib.lua - Go download it and save it in Common!") return end
 if not pcall( require, "DamageLib" ) then PrintChat("You are missing DamageLib.lua - Go download it and save it in Common!") return end
 
-AutoUpdate("/D3ftsu/GoS/master/Jinx.lua","/D3ftsu/GoS/master/Jinx.version","Jinx.lua",1)
+AutoUpdate("/D3ftsu/GoS/master/Jinx.lua","/D3ftsu/GoS/master/Jinx.version","Jinx.lua",2)
 
 local JinxMenu = MenuConfig("Jinx", "Jinx")
 JinxMenu:Menu("Combo", "Combo")
@@ -208,7 +208,6 @@ OnTick(function(myHero)
             end
 	  elseif GetHP(enemy) < getdmg("R", enemy) then 
             Cast(_R,enemy, myHero, UltSpeed(enemy), 0.316, JinxMenu.Combo.R.Max:Value(), 140, 3, true, false, true)
-	    end
 	  end
         end
 
