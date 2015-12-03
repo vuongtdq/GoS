5d5cb6c1b57062896929baa15ddffb819dfa0038
D3ftsu <ahmedtayachon1@gmail.com>
2015-12-03 11:38:23 +0100

    Update Katarina.lua

--- Katarina.lua
+++ Katarina.lua
@@ -4,7 +4,7 @@ require('Inspired')
 require('DeftLib')
 require('DamageLib')
 
-AutoUpdate("/D3ftsu/GoS/master/Katarina.lua","/D3ftsu/GoS/master/Katarina.version","Katarina.lua",6)
+AutoUpdate("/D3ftsu/GoS/master/Katarina.lua","/D3ftsu/GoS/master/Katarina.version","Katarina.lua",7)
 
 local KatarinaMenu = MenuConfig("Katarina", "Katarina")
 KatarinaMenu:Menu("Combo", "Combo")
@@ -52,17 +52,18 @@ KatarinaMenu.Drawings:ColorPick("color", "Color Picker", {255,255,255,0})
 KatarinaMenu.Drawings:Boolean("Text", "Draw Damage Text", true)
 
 OnDraw(function(myHero)
-local pos = GetOrigin(myHero)
-if KatarinaMenu.Drawings.Q:Value() then DrawCircle(pos,675,1,25,GoS.Pink) end
-if KatarinaMenu.Drawings.W:Value() then DrawCircle(pos,375,1,25,GoS.Yellow) end
-if KatarinaMenu.Drawings.E:Value() then DrawCircle(pos,700,1,25,GoS.Blue) end
-if KatarinaMenu.Drawings.R:Value() then DrawCircle(pos,550,1,25,GoS.Green) end
+local col = KatarinaMenu.Drawings.color:Value()
+if KatarinaMenu.Drawings.Q:Value() then DrawCircle(myHeroPos(),675,1,0,col) end
+if KatarinaMenu.Drawings.W:Value() then DrawCircle(myHeroPos(),375,1,0,col) end
+if KatarinaMenu.Drawings.E:Value() then DrawCircle(myHeroPos(),700,1,0,col) end
+if KatarinaMenu.Drawings.R:Value() then DrawCircle(myHeroPos(),550,1,0,col) end
   if KatarinaMenu.Drawings.Text:Value() then
     for _, enemy in pairs(GetEnemyHeroes()) do
       if ValidTarget(enemy) then
-      local drawpos = WorldToScreen(1,GetOrigin(enemy))
+      local enemyPos = GetOrigin(enemy)
+      local drawpos = WorldToScreen(1,enemyPos.x, enemyPos.y, enemyPos.z)
       local enemyText, color = GetDrawText(enemy)
-      DrawText(enemyText, 15, drawpos.x, drawpos.y, color)
+      DrawText(enemyText, 20, drawpos.x, drawpos.y, color)
       end
     end
   end
@@ -108,12 +109,12 @@ local function calcMaxPos(pos)
 	return {x = origin.x + 600 * vectorx / dist ,y = origin.y + 600 * vectory / dist, z = origin.z + 600 * vectorz / dist}
 end
 
-local function ValidTarget2(object)
+local function ValidTarget2( object )
 	local objType = GetObjectType(object)
 	return (objType == Obj_AI_Hero or objType == Obj_AI_Minion) and IsVisible(object)
 end
 
-local findWardSlot = function()
+local findWardSlot = function ()
 	local slot = 0
 	for i,wardItem in pairs(wardItems) do
 	slot = GetItemSlot(myHero,wardItem.id)
@@ -136,7 +137,7 @@ end
 
 local spellLock = nil
 
-function wardJump(pos)
+function wardJump( pos )
 	if not spellLock and IsReady(_E) then
 		if jumpTarget then
 		CastTargetSpell(jumpTarget, _E)
@@ -153,9 +154,9 @@ local function GetJumpTarget()
 	if not IsInDistance2(600, mousePos, GetOrigin(myHero)) then
 	pos = maxPos
 	end
-	for _,Object in pairs(objectList) do
-	  if ValidTarget2(Object) and IsInDistance2(200, GetOrigin(object), pos) then
-	  return Object
+	for _,object in pairs(objectList) do
+	  if ValidTarget2(object) and IsInDistance2(200, GetOrigin(object), pos) then
+	  return object
 	  end
 	end
 	return nil
@@ -164,12 +165,8 @@ end
 local CastingR = false
 local target1 = TargetSelector(675,TARGET_LESS_CAST_PRIORITY,DAMAGE_MAGIC,true,false)
 local target2 = TargetSelector(700,TARGET_LESS_CAST_PRIORITY,DAMAGE_MAGIC,true,false)
-local lastlevel = GetLevel(myHero)-1
 
 OnTick(function(myHero)
-  mousePos = GetMousePos()
-  maxPos = calcMaxPos(mousePos)
-  jumpTarget = GetJumpTarget()
   local target = GetCurrentTarget()
   local Qtarget = target1:GetTarget()
   local Etarget = target2:GetTarget()
@@ -198,15 +195,15 @@ OnTick(function(myHero)
 
   if IOW:Mode() == "Harass" then
  
-      if IsReady(_Q) and KatarinaMenu.Harass.Q:Value() and ValidTarget(Qtarget, 675) and not CastingR then
+      if IsReady(_Q) and KatarinaMenu.Harass.Q:Value() and ValidTarget(Qtarget, 675) then
       CastTargetSpell(Qtarget, _Q)
       end
 	  
-      if IsReady(_W) and KatarinaMenu.Harass.W:Value() and ValidTarget(target, 375) and not CastingR then
+      if IsReady(_W) and KatarinaMenu.Harass.W:Value() and ValidTarget(target, 375) then
       CastSpell(_W)
       end
 	  
-      if IsReady(_E) and KatarinaMenu.Harass.E:Value() and ValidTarget(Etarget, 700) and not CastingR then
+      if IsReady(_E) and KatarinaMenu.Harass.E:Value() and ValidTarget(Etarget, 700) then
       CastTargetSpell(Etarget, _E)
       end
   end
@@ -220,15 +217,15 @@ OnTick(function(myHero)
                   end
                 end
 		
-                if IsReady(_W) and GetHP2(enemy) < getdmg("W",enemy) and ValidTarget(enemy, 375) and not CastingR then 
+                if IsReady(_W) and GetHP2(enemy) < getdmg("W",enemy) and ValidTarget(enemy, 375) then 
 		CastSpell(_W)
 	        end		
 	
-		if IsReady(_Q) and GetHP2(enemy) < getdmg("Q",enemy) and ValidTarget(enemy, 675) and not CastingR then 
+		if IsReady(_Q) and GetHP2(enemy) < getdmg("Q",enemy) and ValidTarget(enemy, 675) then 
 		CastTargetSpell(enemy, _Q)
 		end	
 		
-		if IsReady(_E) and GetHP2(enemy) < getdmg("E",enemy) and ValidTarget(enemy, 700) and not CastingR then 
+		if IsReady(_E) and GetHP2(enemy) < getdmg("E",enemy) and ValidTarget(enemy, 700) then 
 		CastTargetSpell(enemy, _E)
 	        end		
 		
@@ -303,6 +300,10 @@ OnTick(function(myHero)
 
 end
 
+        mousePos = GetMousePos()
+	maxPos = calcMaxPos(mousePos)
+	jumpTarget = GetJumpTarget()
+
 	if not spellLock and wardLock and jumpTarget and IsReady(_E) then
 	CastTargetSpell(jumpTarget, _E)
 	spellLock = GetTickCount()
@@ -325,17 +326,6 @@ end
 	spellObj = nil
 	wardpos = nil
 
-if KatarinaMenu.Misc.Autolvl:Value() then
- if GetLevel(myHero) > lastlevel then
-   if KatarinaMenu.Misc.Autolvltable:Value() == 1 then leveltable = {_Q, _W, _E, _Q, _Q , _R, _Q, _W, _Q , _W, _R, _W, _W, _E, _E, _R, _E, _E}
-   elseif KatarinaMenu.Misc.Autolvltable:Value() == 2 then leveltable = {_Q, _W, _E, _W, _W, _R, _W, _Q, _W, _Q, _R, _Q, _Q, _E, _E, _R, _E, _E}
-   elseif KatarinaMenu.Misc.Autolvltable:Value() == 3 then leveltable = {_Q, _W, _E, _Q, _Q, _R, _Q, _E, _Q, _E, _R, _E, _E, _W, _W, _R, _W, _W,}
-   end
-   DelayAction(function() LevelSpell(leveltable[GetLevel(myHero)]) end, math.random(1000,3000))
-   lastlevel = GetLevel(myHero)
- end
-end
-
 end)
 
 OnProcessSpell(function(unit,spell)
@@ -343,11 +333,6 @@ OnProcessSpell(function(unit,spell)
   CastingR = true
   IOW.movementEnabled = false
   IOW.attacksEnabled = false
-  DelayAction(function()
-  CastingR = false
-  IOW.movementEnabled = true
-  IOW.attacksEnabled = true
-  end, 2500)
   end
 
   if unit == myHero and not spell.name:lower():find("katarina") then
@@ -356,21 +341,17 @@ OnProcessSpell(function(unit,spell)
   end
 end)
 
-OnObjectLoad(function(Object)
-if GetObjectType(Object) == Obj_AI_Hero or GetObjectType(Object) == Obj_AI_Minion then
-  objectList[Object] = Object
+OnCreateObj(function(object)
+  local objType = GetObjectType(object)
+  if objType == Obj_AI_Hero or objType == Obj_AI_Minion then
+  objectList[object] = object
   end
 end)
 
-OnCreateObj(function(Object)
-  if GetObjectType(Object) == Obj_AI_Hero or GetObjectType(Object) == Obj_AI_Minion then
-  objectList[Object] = Object
-  end
-end)
-
-OnDeleteObj(function(Object)
-  if GetObjectType(Object) == Obj_AI_Hero or GetObjectType(Object) == Obj_AI_Minion then
-  objectList[Object] = nil
+OnDeleteObj(function(object)
+  local objType = GetObjectType(object)
+  if objType == Obj_AI_Hero or objType == Obj_AI_Minion then
+  objectList[object] = nil
   end
 end)
 
