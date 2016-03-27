d3b67d4caa7b77bbd9957619f327e9fb06c85aec
D3ftsu <ahmedtayachon1@gmail.com>
2016-03-27 12:26:37 +0100

    Update ChallengerStream.lua

--- ChallengerStream.lua
+++ ChallengerStream.lua
@@ -1,2 +1,2 @@
-_G.DrawText, _G.PrintChat, _G.DrawLine, _G.DrawCircle, _G.FillRect, _G.PrintChat, _G.DrawSprite, _G.PlaySound, _G.print = function() end, function() end, function() end, function() end, function() end, function() end, function() end, function() end, function() end
+_G.DrawText, _G.DrawLine, _G.DrawCircle, _G.FillRect, _G.PrintChat, _G.DrawSprite, _G.PlaySound, _G.print = function() end, function() end, function() end, function() end, function() end, function() end, function() end, function() end
 -- 1 line code :nod:
